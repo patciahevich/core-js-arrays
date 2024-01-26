1730e50e615310b01ff899560ef12c71819e413c
Volha Patciahevich <zaprudskaya@gmail.com>
2024-01-26 16:23:54 +0300

    feat: solve getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -394,10 +394,10 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const result = arr.filter((item) => Boolean(item) !== true);
+  return result.length;
 }
-
 /**
  * Creates an identity matrix of the specified size.
  *
