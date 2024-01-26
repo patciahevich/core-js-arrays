8856de0d815b96691a2317073d80eea1423d78ca
Volha Patciahevich <zaprudskaya@gmail.com>
2024-01-26 16:19:45 +0300

    feat: solve generateOdds task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -360,8 +360,10 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const result = new Array(len).fill();
+
+  return result.map((item, index) => index * 2 + 1);
 }
 
 /**
