2dc61e7567d781caa0a4a5e801bf553875c8cd5f
Volha Patciahevich <zaprudskaya@gmail.com>
2024-01-26 12:09:46 +0300

    feat: solve getIntervalArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,10 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const arr = new Array(end - start + 1).fill(start);
+
+  return Array.from(arr, (item, index) => item + index);
 }
 
 /**
