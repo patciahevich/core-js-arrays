acce9585fa79121705eacd5c5302721984b9e493
Volha Patciahevich <zaprudskaya@gmail.com>
2024-01-27 19:38:19 +0300

    feat: solve createChunks task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -344,8 +344,24 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  let chunk = [];
+  let count = 0;
+  const result = arr.reduce((acc, item) => {
+    chunk.push(item);
+    count += 1;
+    if (count === chunkSize) {
+      acc.push(chunk);
+      chunk = [];
+      count = 0;
+    }
+    return acc;
+  }, []);
+  if (chunk.length > 0) {
+    result.push(chunk);
+  }
+
+  return result;
 }
 
 /**
